86675536aec746da4b3e1d4e9a9f95d208ec8912
Opilane TTHK <opilane@tthk.ee>
2020-03-13 12:09:30 +0200

    the studies have been developed

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 const items = [];
+const itemsStudy = [];
 
 app.get('/', (req, res)=>{
     let today = new Date();
@@ -28,10 +29,23 @@ app.get('/', (req, res)=>{
 
 app.post('/', (req, res)=>{
     let item = req.body.newItem;
-    items.push(item);
+    if(req.body.list === "Study TODO"){
+        itemsStudy.push(item);
+        res.redirect('/study');
+    } else {
+        items.push(item)
+        res.redirect('/');
+    }
     console.log(items);
-    res.redirect('/');
-})
+});
+
+app.get('/study', (req, res)=>{
+    res.render('index', {
+        listTitle: "Study TODO",
+        newListItems: itemsStudy
+    })
+});
+
 
 app.listen(port, ()=>{
     console.log("Server is vibing on port " + port);
